ad26d847a4b838678375764fcfa200c93a6c58c7
Lakshita purohit <2326226.lakshita.purohit@vvwusurat.ac.in>
2026-07-15 05:52:39 +0000

    Datatypes.js

--- Q1_basics/test.js
+++ Q1_basics/test.js
@@ -1,1 +1,21 @@
-console.log("hitesh")
+const accountId =144553 //not reassign
+let accountEmail ="lakshita@google.com"
+var accountPassword ="123456" 
+accountCity ="jaipur"
+
+let accountState;
+
+// accoundId = 2 // not allowed 
+accountEmail = "hc@hc.com"
+accountPassword = "3214321"
+accountCity = "surat"
+
+console.log(accountId);
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail,accountPassword, accountCity, accountState ])
+
+
+                                                                                                                                                                                            
