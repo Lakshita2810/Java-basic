8ff879ab5d44d1576e35ef9f7c43213ef3df6e51
Lakshita purohit <2326226.lakshita.purohit@vvwusurat.ac.in>
2026-07-15 05:51:46 +0000

    Datatypes

---
+++ Q1_basics/Datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+// alert( 3 + 3) we are using node.js , not browser
+// code readability should be high
+
+// DATATYPES :- PRIMITIVE DATATYPES
+// number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique 
+
+// NON PRIMITIVE DATATYPES
+// object
+console.log(typeof("hitesh"));
+
+
