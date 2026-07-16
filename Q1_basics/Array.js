f55c3cd472d4c1db00e1bedf1fc78f0053c0e269
Lakshita purohit <2326226.lakshita.purohit@vvwusurat.ac.in>
2026-07-16 05:43:40 +0000

    array

---
+++ Q1_basics/Array.js
@@ -0,0 +1,41 @@
+const myArr = [1,2,3,4,6,7,0,8]
+// console.log(myArr[3]);
+
+// const myHeros = ["ritiik roshn", "batman"]
+// console.log(myHeros);
+
+// array methods
+
+// myArr.push(6)
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+
+// myArr.unshift(9)
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+
+// console.log(myArr.includes(9));
+
+// console.log(myArr.indexOf(3));
+console.log(myArr);
+
+// const newArr = myArr.join()
+// console.log(newArr);
+
+// Slice, Splice
+
+const myn1 = myArr.slice(0,3)
+console.log(myn1);
+console.log("B", myArr);
+const myn2 = myArr.splice(1,1, 20)
+console.log(myn2);
+
+
+// -----------------------------------------
+
