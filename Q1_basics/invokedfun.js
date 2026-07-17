c5391890d6168aaddbec8fa0615819bdb05b55c9
Lakshita purohit <2326226.lakshita.purohit@vvwusurat.ac.in>
2026-07-17 06:50:34 +0000

    IIFE

---
+++ Q1_basics/invokedfun.js
@@ -0,0 +1,18 @@
+// (function chai() {
+//     console.log(`hellooo`);
+    
+// }) ();
+
+
+// ( () =>  {
+//     console.log(`db connnected`);
+    
+// }) ()
+
+((name) => { 
+    console.log(`the world loves ${name}`); 
+})('akshu');
+
+( (name) =>
+     { console.log(`thw world love the ${name}`); 
+})('akshu')
